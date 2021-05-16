4eafabe16415281e0e57b5fdd9c94ce80d6320e4
DoanDuc <duc@ktcc.co.jp>
2021-05-16 19:36:22 +0900

    remove footer

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
 	<!-- To the right -->
-	<div class="float-right d-none d-sm-inline">
+	<!-- <div class="float-right d-none d-sm-inline">
 		Developed by <a href="https://www.facebook.com/bibitailor/" target="_blank">Dukinfotech</a>
-	</div>
+	</div> -->
 	<!-- Default to the left -->
 	<div>Copyright &copy; {{ date('Y') }} All rights reserved</div>
 </footer>
